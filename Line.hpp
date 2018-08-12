542c2b6ca162e808afab8696c881f58b5d4f1d5b
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2018-08-13 07:44:18 +1000

    Passing regex argument. Refactored argument reading code.

--- Line.hpp
+++ Line.hpp
@@ -8,40 +8,44 @@
 
 #include "ArgManager.hpp"
 
+typedef std::string str;
+typedef std::vector<unsigned> uvec;
+
 class Line {
 public:
 	Line() {}
-	Line(const std::string& line);
-	void split(const std::string& delimiter);
-	void process(const std::vector<unsigned>& re_fields, const std::string& regex, bool inverse);
-	std::string join(const std::string& delimiter, const std::vector<unsigned>& fields);
+	Line(const str& line);
+	void split(const str& delimiter);
+	void process(const uvec& re_fields, const str& re_search, const str& re_replace, bool inverse);
+	str join(const str& delimiter, const uvec& fields);
 
 private:
 	unsigned m_line_num;
-	std::string m_line;
-	std::string m_joined;
-	std::vector<std::string> m_parts;
+	str m_line;
+	str m_joined;
+	std::vector<str> m_parts;
 
 private:
-	void joinList(const std::string& delimiter, const std::vector<unsigned>& fields);
-	void joinAll(const std::string& delimiter);
+	void joinList(const str& delimiter, const uvec& fields);
+	void joinAll(const str& delimiter);
 	unsigned getNumParts() const;
-	void processPart(int part_num, const std::string& regex);
-	bool find(unsigned needle, const std::vector<unsigned>& haystack) const;
+	void processPart(int part_num, const str& re_search, const str& re_replace);
+	void processPart(int part_num, const str& regex);
+	bool find(unsigned needle, const uvec& haystack) const;
 };
 
-Line::Line(const std::string& line) : m_line(line)
+Line::Line(const str& line) : m_line(line)
 {
 	static auto line_num = 0;
 	m_line_num = ++line_num;
 }
 
-void Line::split(const std::string& delimiter)
+void Line::split(const str& delimiter)
 {
 	auto pos_start = 0u;
-	auto pos_end = std::string::npos;
+	auto pos_end = str::npos;
 
-	while((pos_end = m_line.find(delimiter,pos_start)) != std::string::npos) {
+	while((pos_end = m_line.find(delimiter,pos_start)) != str::npos) {
 		m_parts.push_back(m_line.substr(pos_start, (pos_end-pos_start)));
 		pos_start = pos_end + 1;
 	}
@@ -53,7 +57,7 @@ unsigned Line::getNumParts() const
 	return m_parts.size();
 }
 
-void Line::process(const std::vector<unsigned>& re_fields, const std::string& regex, bool inverse)
+void Line::process(const uvec& re_fields, const str& re_search, const str& re_replace, bool inverse)
 {
 	for (auto i = 0u; i<getNumParts(); ++i) {
 		bool process = false;
@@ -67,24 +71,24 @@ void Line::process(const std::vector<unsigned>& re_fields, const std::string& re
 		}
 
 		if (process) {
-			processPart(i, regex);
+			processPart(i, re_search, re_replace);
 		}
 	}
 }
 
-bool Line::find(unsigned needle, const std::vector<unsigned>& haystack) const
+bool Line::find(unsigned needle, const uvec& haystack) const
 {
 	return std::find(haystack.begin(), haystack.end(), needle) != haystack.end();
 }
 
-void Line::processPart(int part_num, const std::string& regex)
+void Line::processPart(int part_num, const str& re_search, const str& re_replace)
 {
-	std::regex r(regex);
-	m_parts[part_num] = std::regex_replace(m_parts[part_num], r, "X");
+	std::regex r(re_search);
+	m_parts[part_num] = std::regex_replace(m_parts[part_num], r, re_replace);
 }
 
 
-std::string Line::join(const std::string& delimiter, const std::vector<unsigned>& fields)
+str Line::join(const str& delimiter, const uvec& fields)
 {
 
 	if (fields.size() == 0) {
@@ -95,7 +99,7 @@ std::string Line::join(const std::string& delimiter, const std::vector<unsigned>
 	return m_joined;
 }
 
-void Line::joinList(const std::string& delimiter, const std::vector<unsigned>& fields)
+void Line::joinList(const str& delimiter, const uvec& fields)
 {
 	bool first = true;
 	for (auto i : fields) {
@@ -107,7 +111,7 @@ void Line::joinList(const std::string& delimiter, const std::vector<unsigned>& f
 	}
 }
 
-void Line::joinAll(const std::string& delimiter)
+void Line::joinAll(const str& delimiter)
 {
 	for (auto i = 0u; i<getNumParts(); ++i) {
 		m_joined += (i == 0) ? "" : delimiter;
