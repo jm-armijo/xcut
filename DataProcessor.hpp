be4b8cea2d82ec3622f4271d6c90f3f7939dc0ca
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2018-08-21 17:07:16 +1000

    Bug fixes: - Locking to check line number (was causing seg fault) - Changin do while to plain while loops.

--- DataProcessor.hpp
+++ DataProcessor.hpp
@@ -34,6 +34,7 @@ private:
 	std::string getOutputLine(unsigned line_num);
 	void printOutputSorted();
 	void printOutputUnsorted();
+	unsigned countLines(unsigned line_num);
 
 private:
 	DataProcessor() {}
@@ -61,14 +62,13 @@ void DataProcessor::processLines(const ArgManager& arg_manager)
 	auto line_num = 0u;
 	std::vector<std::thread> threads;
 
-	// Try at least once in case we stopped reading before reaching this point.
-	do {
-		while (m_lines_in.size() > 0) {
+	// If stopped reading we may still have some items to process
+	while (m_still_reading || m_lines_in.size() > 0) {
+		if (m_lines_in.size() > 0) {
 			auto val = dequeueInput();
 			threads.push_back(std::thread(&DataProcessor::processLine, this, line_num++, val, arg_manager));
 		}
-	} while (m_still_reading || m_lines_in.size() > 0);
-	// If stopped reading we may still have some items to process
+	}
 
 	// Wait for all threads to finish.
 	for (auto& t : threads) {
@@ -90,13 +90,13 @@ void DataProcessor::processLine(unsigned line_num, std::string src_line, const A
 
 void DataProcessor::printOutput(const ArgManager& arg_manager)
 {
-	do {
+	while (m_still_processing || m_lines_out.size() > 0) {
 		if (arg_manager.sortOutput()) {
 			printOutputSorted();
 		} else {
 			printOutputUnsorted();
 		}
-	} while (m_still_processing || m_lines_out.size() > 0);
+	}
 
 	return;
 }
@@ -104,12 +104,20 @@ void DataProcessor::printOutput(const ArgManager& arg_manager)
 void DataProcessor::printOutputSorted()
 {
 	static auto line_num = 0u;
-	if (m_lines_out.count(line_num) > 0) {
-		auto val = getOutputLine(line_num++);
+	auto count = countLines(line_num);
+	if (count > 0) {
+		auto val = getOutputLine(line_num);
+		++line_num;
 		std::cout << val << "\n";
 	}
 
 	return;
+}
+
+unsigned DataProcessor::countLines(unsigned line_num)
+{
+	std::lock_guard<std::mutex> guard(m_mtx_lines_out);
+	return m_lines_out.count(line_num);
 }
 
 void DataProcessor::printOutputUnsorted()
@@ -159,6 +167,6 @@ std::string DataProcessor::getOutputLine(unsigned line_num)
 	auto val = m_lines_out.at(line_num);
 	m_lines_out.erase(line_num);
 	return val;
-
 }
+
 #endif //JM_INPUT_READER_HPP
