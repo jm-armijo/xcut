68c41036faa063f914ea95b8637d01d187d463a1
Jose Miguel Armijo <jm.armijo.f@gmail.com>
2018-08-29 21:25:46 +1000

    Converting \t to 4 spaces

--- ArgManager.hpp
+++ ArgManager.hpp
@@ -12,232 +12,232 @@ enum class Argument {h, d, f, p, i, x, s};
 
 class ArgManager {
 public:
-	// Entry point to start reading and processing arguments
-	void readArgs(int argc, char **argv);
+    // Entry point to start reading and processing arguments
+    void readArgs(int argc, char **argv);
 
-	// Getter functions
-	bool getHelp() const;
-	void printHelp() const;
-	std::string getDelimiter() const;
-	std::string getRegExSearch() const;
-	std::string getRegExReplace() const;
-	std::vector<unsigned> getRegExFields() const;
-	std::vector<unsigned> getFields() const;
-	bool getInverseRegExFields() const;
-	bool sortOutput() const {return m_sort;}
-
-private:
-	// Stores the overal status of the argument reading operation
-	bool m_read_ok = true;
-
-	// Options storage
-	bool m_help = false;
-	bool m_re_invert_fields = false;
-	bool m_sort = false;
-	std::string m_delimiter = " ";
-	std::string m_regex;
-	std::string m_re_search;
-	std::string m_re_replace;
-	std::vector<unsigned> m_re_fields;
-	std::vector<unsigned> m_fields;
+    // Getter functions
+    bool getHelp() const;
+    void printHelp() const;
+    std::string getDelimiter() const;
+    std::string getRegExSearch() const;
+    std::string getRegExReplace() const;
+    std::vector<unsigned> getRegExFields() const;
+    std::vector<unsigned> getFields() const;
+    bool getInverseRegExFields() const;
+    bool sortOutput() const {return m_sort;}
 
 private:
-	// Methods to read arguments
-	bool isUnaryOption(const std::string& option) const;
-	bool isBinaryOption(const std::string& option) const;
-	void setValue(const std::string& option, const std::string& value);
-	void flagError(const std::string& msg);
-	void validateArgs();
+    // Stores the overal status of the argument reading operation
+    bool m_read_ok = true;
 
-	// Methods to process arguments
-	std::vector<unsigned> splitFields(const std::string& arg_val) const;
-	std::tuple<std::string, std::string> splitRegex(const std::string& arg_val) const;
+    // Options storage
+    bool m_help = false;
+    bool m_re_invert_fields = false;
+    bool m_sort = false;
+    std::string m_delimiter = " ";
+    std::string m_regex;
+    std::string m_re_search;
+    std::string m_re_replace;
+    std::vector<unsigned> m_re_fields;
+    std::vector<unsigned> m_fields;
+
+private:
+    // Methods to read arguments
+    bool isUnaryOption(const std::string& option) const;
+    bool isBinaryOption(const std::string& option) const;
+    void setValue(const std::string& option, const std::string& value);
+    void flagError(const std::string& msg);
+    void validateArgs();
+
+    // Methods to process arguments
+    std::vector<unsigned> splitFields(const std::string& arg_val) const;
+    std::tuple<std::string, std::string> splitRegex(const std::string& arg_val) const;
 
 };
 
 void ArgManager::readArgs(int argc, char **argv)
 {
-	auto state = State::arg;
-	std::string option = "";
+    auto state = State::arg;
+    std::string option = "";
 
-	for(auto i=1; i<argc; ++i) {
-		if (state == State::arg) {
+    for(auto i=1; i<argc; ++i) {
+        if (state == State::arg) {
 
-			option = argv[i];
-			if (isUnaryOption(option)) {
-				setValue(option, "1");
-			} else if (isBinaryOption(option)) {
-				state = State::val;
-			} else {
-				flagError("Invalid option '" + option + "'");
-				i = argc; // exit the loop
-			}
+            option = argv[i];
+            if (isUnaryOption(option)) {
+                setValue(option, "1");
+            } else if (isBinaryOption(option)) {
+                state = State::val;
+            } else {
+                flagError("Invalid option '" + option + "'");
+                i = argc; // exit the loop
+            }
 
-		} else {
-			setValue(option, argv[i]);
-			state = State::arg;
-		}
-	}
+        } else {
+            setValue(option, argv[i]);
+            state = State::arg;
+        }
+    }
 
-	if (m_read_ok && state == State::val) {
-		flagError("Expected value for option '" + option + "'");
-	} else if (m_read_ok) {
-		validateArgs();
-	}
+    if (m_read_ok && state == State::val) {
+        flagError("Expected value for option '" + option + "'");
+    } else if (m_read_ok) {
+        validateArgs();
+    }
 }
 
 bool ArgManager::isUnaryOption(const std::string& option) const
 {
-	static const std::vector<std::string> unary = {"-h", "-i", "-s"};
-	return (std::find(unary.begin(), unary.end(), option) != unary.end());
+    static const std::vector<std::string> unary = {"-h", "-i", "-s"};
+    return (std::find(unary.begin(), unary.end(), option) != unary.end());
 }
 
 bool ArgManager::isBinaryOption(const std::string& option) const
 {
-	static const std::vector<std::string> binary = {"-d", "-f", "-p", "-x"};
-	return (std::find(binary.begin(), binary.end(), option) != binary.end());
+    static const std::vector<std::string> binary = {"-d", "-f", "-p", "-x"};
+    return (std::find(binary.begin(), binary.end(), option) != binary.end());
 }
 
 void ArgManager::setValue(const std::string& option, const std::string& value)
 {
-	if (option == "-h") {
-		m_help = (value == "1");
-	} else if (option == "-i") {
-		m_re_invert_fields = (value == "1");
-	} else if (option == "-s") {
-		m_sort = (value == "1");
-	} else if (option == "-d") {
-		m_delimiter = value;
-	} else if (option == "-f") {
-		m_fields = splitFields(value);
-	} else if (option == "-p") {
-		m_re_fields = splitFields(value);
-	} else if (option == "-x") {
-		m_regex = value;
-		std::tie(m_re_search, m_re_replace) = splitRegex(value);
-	} else {
-		std::runtime_error("Unhandled option '" + option + "'");
-	}
+    if (option == "-h") {
+        m_help = (value == "1");
+    } else if (option == "-i") {
+        m_re_invert_fields = (value == "1");
+    } else if (option == "-s") {
+        m_sort = (value == "1");
+    } else if (option == "-d") {
+        m_delimiter = value;
+    } else if (option == "-f") {
+        m_fields = splitFields(value);
+    } else if (option == "-p") {
+        m_re_fields = splitFields(value);
+    } else if (option == "-x") {
+        m_regex = value;
+        std::tie(m_re_search, m_re_replace) = splitRegex(value);
+    } else {
+        std::runtime_error("Unhandled option '" + option + "'");
+    }
 }
 
 void ArgManager::flagError(const std::string& msg)
 {
-	std::cerr << "xcut: " << msg << "\n" << std::endl;
-	m_read_ok = false;
+    std::cerr << "xcut: " << msg << "\n" << std::endl;
+    m_read_ok = false;
 }
 
 void ArgManager::validateArgs()
 {
-	if (m_re_fields.size() > 0 && m_regex == "") {
-		flagError("Option -p requires option -x.");
-	} else if (m_re_invert_fields && m_re_fields.size() == 0) {
-		flagError("Option -i requires option -p.");
-	} else if (m_regex != "" && m_re_search == "") {
-		flagError("Option -x requires a valid non-empty search pattern.");
-	}
+    if (m_re_fields.size() > 0 && m_regex == "") {
+        flagError("Option -p requires option -x.");
+    } else if (m_re_invert_fields && m_re_fields.size() == 0) {
+        flagError("Option -i requires option -p.");
+    } else if (m_regex != "" && m_re_search == "") {
+        flagError("Option -x requires a valid non-empty search pattern.");
+    }
 }
 
 
 // Getter functions
 bool ArgManager::getHelp() const
 {
-	return (m_help || !m_read_ok);
+    return (m_help || !m_read_ok);
 }
 
 void ArgManager::printHelp() const
 {
-	std::ostream& out = m_help ? std::cout : std::cerr;
+    std::ostream& out = m_help ? std::cout : std::cerr;
 
-	out << "Usage: xcut OPTION... < [FILE]...\n";
-	out << "From each FILE, replaces text on all or selected parts of lines using PATTERN,\n";
-	out << "and print all or selected parts to standard output.\n\n";
+    out << "Usage: xcut OPTION... < [FILE]...\n";
+    out << "From each FILE, replaces text on all or selected parts of lines using PATTERN,\n";
+    out << "and print all or selected parts to standard output.\n\n";
 
-	out << "Example: xcut -f 1,2 -x 's/\\d/<num>/' < file.txt\n\n";
+    out << "Example: xcut -f 1,2 -x 's/\\d/<num>/' < file.txt\n\n";
 
-	out << "Options\n";
-	out << "  -d DELIM    Use DELIM instead of SPACE for field delimiter.\n";
-	out << "  -f FIELDS   Comma separated list of fiels to print (1-index base).\n";
-	out << "  -p FIELDS   Comma separated list of fiels to apply PATTERN to. (1-index base)\n";
-	out << "  -x PATTERN  sed like Regular Expression to be applied on all or specified parts.\n";
-	out << "  -i          Apply PATTERN to inversed -p list\n";
-	out << "  -s          Output lines sorted in the original order.\n";
-	out << "  -h          This help\n";
+    out << "Options\n";
+    out << "  -d DELIM    Use DELIM instead of SPACE for field delimiter.\n";
+    out << "  -f FIELDS   Comma separated list of fiels to print (1-index base).\n";
+    out << "  -p FIELDS   Comma separated list of fiels to apply PATTERN to. (1-index base)\n";
+    out << "  -x PATTERN  sed like Regular Expression to be applied on all or specified parts.\n";
+    out << "  -i          Apply PATTERN to inversed -p list\n";
+    out << "  -s          Output lines sorted in the original order.\n";
+    out << "  -h          This help\n";
 
-	out << "\nAll options are optional, except in these cases:\n";
-	out << "    If option -p is used, option -x becomes mandatory\n";
-	out << "    If option -i is used, option -x becomes mandatory\n";
-	out << std::flush;
+    out << "\nAll options are optional, except in these cases:\n";
+    out << "    If option -p is used, option -x becomes mandatory\n";
+    out << "    If option -i is used, option -x becomes mandatory\n";
+    out << std::flush;
 }
 
 std::string ArgManager::getDelimiter() const
 {
-	return m_delimiter;
+    return m_delimiter;
 }
 
 std::vector<unsigned> ArgManager::getFields() const
 {
-	return m_fields;
+    return m_fields;
 }
 
 std::vector<unsigned> ArgManager::getRegExFields() const
 {
-	return m_re_fields;
+    return m_re_fields;
 }
 
 std::string ArgManager::getRegExSearch() const
 {
-	return m_re_search;
+    return m_re_search;
 }
 
 std::string ArgManager::getRegExReplace() const
 {
-	return m_re_replace;
+    return m_re_replace;
 }
 
 bool ArgManager::getInverseRegExFields() const
 {
-	return m_re_invert_fields;
+    return m_re_invert_fields;
 }
 
 std::vector<unsigned> ArgManager::splitFields(const std::string& arg_val) const
 {
-	auto pos_start = 0u;
-	auto pos_end   = std::string::npos;
+    auto pos_start = 0u;
+    auto pos_end   = std::string::npos;
 
-	std::vector<unsigned> fields;
-	while ((pos_end = arg_val.find(",", pos_start)) != std::string::npos) {
-		auto val = std::stoul(arg_val.substr(pos_start, (pos_end-pos_start)));
-		fields.push_back(val);
-		pos_start = pos_end+1;
-	}
-	fields.push_back(std::stoi(arg_val.substr(pos_start, (pos_end-pos_start))));
-	
-	return fields;
+    std::vector<unsigned> fields;
+    while ((pos_end = arg_val.find(",", pos_start)) != std::string::npos) {
+        auto val = std::stoul(arg_val.substr(pos_start, (pos_end-pos_start)));
+        fields.push_back(val);
+        pos_start = pos_end+1;
+    }
+    fields.push_back(std::stoi(arg_val.substr(pos_start, (pos_end-pos_start))));
+    
+    return fields;
 }
 
 std::tuple<std::string, std::string> ArgManager::splitRegex(const std::string& arg_val) const
 {
-	static const std::regex match_re("s/(.*)/(.*)/");
+    static const std::regex match_re("s/(.*)/(.*)/");
 
-	std::string search;
-	std::string replace;
+    std::string search;
+    std::string replace;
 
-	std::smatch parts;
-	if (std::regex_match(arg_val, parts, match_re)) {
-		search = parts[1];
+    std::smatch parts;
+    if (std::regex_match(arg_val, parts, match_re)) {
+        search = parts[1];
 
-		// Properly escape sequence \/
-		std::regex clean_slash("\\\\(/)");
-		search = std::regex_replace(search, clean_slash, "$01");
+        // Properly escape sequence \/
+        std::regex clean_slash("\\\\(/)");
+        search = std::regex_replace(search, clean_slash, "$01");
 
-		// Properly escape double backslash sequence (\\)
-		std::regex clean_backslash("\\\\\\\\");
-		search = std::regex_replace(search, clean_backslash, "\\\\\\\\");
+        // Properly escape double backslash sequence (\\)
+        std::regex clean_backslash("\\\\\\\\");
+        search = std::regex_replace(search, clean_backslash, "\\\\\\\\");
 
-		replace = parts[2];
-	}
+        replace = parts[2];
+    }
 
-	return std::make_tuple(search, replace);
+    return std::make_tuple(search, replace);
 }
 
 #endif //JM_ARG_MANAGER_HPP
--- Line.hpp
+++ Line.hpp
@@ -13,125 +13,125 @@ typedef std::vector<unsigned> uvec;
 
 class Line {
 public:
-	Line() {}
-	Line(const str& line);
-	void split(const str& delimiter);
-	std::string process(const ArgManager& arg_manager);
-	str join(const str& delimiter, const uvec& fields);
+    Line() {}
+    Line(const str& line);
+    void split(const str& delimiter);
+    std::string process(const ArgManager& arg_manager);
+    str join(const str& delimiter, const uvec& fields);
 
 private:
-	unsigned m_line_num;
-	str m_line;
-	str m_joined;
-	std::vector<str> m_parts;
+    unsigned m_line_num;
+    str m_line;
+    str m_joined;
+    std::vector<str> m_parts;
 
 private:
-	void joinList(const str& delimiter, const uvec& fields);
-	void joinAll(const str& delimiter);
-	unsigned getNumParts() const;
-	void processPart(int part_num, const str& re_search, const str& re_replace);
-	void processPart(int part_num, const str& regex);
-	bool find(unsigned needle, const uvec& haystack) const;
+    void joinList(const str& delimiter, const uvec& fields);
+    void joinAll(const str& delimiter);
+    unsigned getNumParts() const;
+    void processPart(int part_num, const str& re_search, const str& re_replace);
+    void processPart(int part_num, const str& regex);
+    bool find(unsigned needle, const uvec& haystack) const;
 };
 
 Line::Line(const str& line) : m_line(line)
 {
-	static auto line_num = 0;
-	m_line_num = ++line_num;
+    static auto line_num = 0;
+    m_line_num = ++line_num;
 }
 
 void Line::split(const str& delimiter)
 {
-	auto pos_start = 0u;
-	auto pos_end = str::npos;
+    auto pos_start = 0u;
+    auto pos_end = str::npos;
 
-	while((pos_end = m_line.find(delimiter,pos_start)) != str::npos) {
-		m_parts.push_back(m_line.substr(pos_start, (pos_end-pos_start)));
-		pos_start = pos_end + 1;
-	}
-	m_parts.push_back(m_line.substr(pos_start, (pos_end-pos_start)));
+    while((pos_end = m_line.find(delimiter,pos_start)) != str::npos) {
+        m_parts.push_back(m_line.substr(pos_start, (pos_end-pos_start)));
+        pos_start = pos_end + 1;
+    }
+    m_parts.push_back(m_line.substr(pos_start, (pos_end-pos_start)));
 }
 
 unsigned Line::getNumParts() const
 {
-	return m_parts.size();
+    return m_parts.size();
 }
 
 std::string Line::process(const ArgManager& arg_manager)
 {
-	auto delimiter  = arg_manager.getDelimiter();
-	auto fields     = arg_manager.getFields();
-	auto re_fields  = arg_manager.getRegExFields();
-	auto re_search  = arg_manager.getRegExSearch();
-	auto re_replace = arg_manager.getRegExReplace();
-	auto inverse    = arg_manager.getInverseRegExFields();
+    auto delimiter  = arg_manager.getDelimiter();
+    auto fields     = arg_manager.getFields();
+    auto re_fields  = arg_manager.getRegExFields();
+    auto re_search  = arg_manager.getRegExSearch();
+    auto re_replace = arg_manager.getRegExReplace();
+    auto inverse    = arg_manager.getInverseRegExFields();
 
-	// split the word
-	split(delimiter);
+    // split the word
+    split(delimiter);
 
-	for (auto i = 0u; i<getNumParts(); ++i) {
-		bool process = false;
+    for (auto i = 0u; i<getNumParts(); ++i) {
+        bool process = false;
 
-		if (re_fields.size() == 0) {
-			process = true;
-		} else if (find(i+1, re_fields)) {
-			process = !inverse;
-		} else {
-			process = inverse;
-		}
+        if (re_fields.size() == 0) {
+            process = true;
+        } else if (find(i+1, re_fields)) {
+            process = !inverse;
+        } else {
+            process = inverse;
+        }
 
-		if (process) {
-			processPart(i, re_search, re_replace);
-		}
-	}
+        if (process) {
+            processPart(i, re_search, re_replace);
+        }
+    }
 
-	// join requested fields and save string
-	return join(delimiter, fields);
+    // join requested fields and save string
+    return join(delimiter, fields);
 }
 
 bool Line::find(unsigned needle, const uvec& haystack) const
 {
-	return std::find(haystack.begin(), haystack.end(), needle) != haystack.end();
+    return std::find(haystack.begin(), haystack.end(), needle) != haystack.end();
 }
 
 void Line::processPart(int part_num, const str& re_search, const str& re_replace)
 {
-	try {
-		static const std::regex r(re_search);
-		m_parts[part_num] = std::regex_replace(m_parts[part_num], r, re_replace);
-	} catch (...) {}
+    try {
+        static const std::regex r(re_search);
+        m_parts[part_num] = std::regex_replace(m_parts[part_num], r, re_replace);
+    } catch (...) {}
 }
 
 
 str Line::join(const str& delimiter, const uvec& fields)
 {
 
-	if (fields.size() == 0) {
-		joinAll(delimiter);
-	} else {
-		joinList(delimiter, fields);
-	}
-	return m_joined;
+    if (fields.size() == 0) {
+        joinAll(delimiter);
+    } else {
+        joinList(delimiter, fields);
+    }
+    return m_joined;
 }
 
 void Line::joinList(const str& delimiter, const uvec& fields)
 {
-	bool first = true;
-	for (auto i : fields) {
-		if (i<=getNumParts()) {
-			m_joined += first ? "" : delimiter;
-			m_joined += m_parts[i-1];
-			first = false;
-		}
-	}
+    bool first = true;
+    for (auto i : fields) {
+        if (i<=getNumParts()) {
+            m_joined += first ? "" : delimiter;
+            m_joined += m_parts[i-1];
+            first = false;
+        }
+    }
 }
 
 void Line::joinAll(const str& delimiter)
 {
-	for (auto i = 0u; i<getNumParts(); ++i) {
-		m_joined += (i == 0) ? "" : delimiter;
-		m_joined += m_parts[i];
-	}
+    for (auto i = 0u; i<getNumParts(); ++i) {
+        m_joined += (i == 0) ? "" : delimiter;
+        m_joined += m_parts[i];
+    }
 }
 
 #endif //JM_LINE_HPP
