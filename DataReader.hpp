fbc3e2d3466eda52a8bb0a95311c11c08933ae51
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2018-09-05 07:50:14 +1000

    Reading from std::in or from input files

--- DataReader.hpp
+++ DataReader.hpp
@@ -1,6 +1,9 @@
 #ifndef JM_DATA_READER_HPP
 #define JM_DATA_READER_HPP
 
+#include <iostream>
+#include <fstream>
+#include <vector>
 #include "DataQueue.hpp"
 #include "Worker.hpp"
 
@@ -11,25 +14,42 @@ public:
 
 private:
     DataQueue& m_queue;
+    std::vector<std::string> m_files;
 
 private:
     void do_job();
     DataReader() = delete;
     void readStream();
+    void readFromStream(std::istream& in);
 };
 
 DataReader::DataReader(const std::atomic<Status> &status, const Arguments& args, DataQueue& queue) :
     Worker(status, args), m_queue(queue)
 {
+    m_files = m_args.find_all_matching("file");
 }
 
 void DataReader::do_job()
 {
-    std::string line;
-    while(std::getline(std::cin, line)) {
-        m_queue.push(line);
+    if (m_files.empty()) {
+        std::istream& in = std::cin;
+        readFromStream(in);
+    } else {
+        for (auto& file : m_files) {
+            std::ifstream in (file, std::ifstream::in);
+            readFromStream(in);
+            in.close();
+        }
     }
     m_done = true;
+}
+
+void DataReader::readFromStream(std::istream& in)
+{
+    std::string line;
+    while(std::getline(in, line)) {
+        m_queue.push(line);
+    }
 
     return;
 }
